ae803a03f604179289ac3c04c94f546cd1b3464b
Ocawesome101 <ocawesome101@gmail.com>
2021-09-24 20:52:50 -0400

    more work on CEX loader

--- src/exec/cex.lua
+++ src/exec/cex.lua
@@ -15,9 +15,39 @@ do
       O_EXEC = true,
       O_RDONLY = true
     })
+    
+    if not handle then
+      return nil, err
+    end
+    
+    local data = k.syscalls.read(handle, math.huge)
+    k.syscalls.close(handle)
+    
+    return data
   end
 
-  local function parse_cex(string)
+  local _flags = {
+    lua53 = 0x1,
+    static = 0x2,
+    boot = 0x4,
+    exec = 0x8,
+    library = 0x10,
+  }
+
+  local function parse_cex(str)
+    local header, str = k.util.pop(str, 4)
+    if header ~= "onyC" then
+      return nil, "invalid magic number"
+    end
+
+    local flags, str = k.util.pop(str, 1)
+    flags = flags:byte()
+    local osid, str = k.util.pop(str, 1)
+    osid = osid:byte()
+
+    if osid ~= 0 and isod ~= 255 then
+      return nil, "bad OSID"
+    end
   end
 
   local function load_cex(file)
