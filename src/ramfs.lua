5539df93e55c024143a453a0946dfa3cb73f1fee
Ocawesome101 <ocawesome101@gmail.com>
2021-10-03 21:16:34 -0400

    change :fdlist() to :list();  ramfs has :list() now; made ramfs code slightly prettier (more whitespace :P)

--- src/ramfs.lua
+++ src/ramfs.lua
@@ -6,6 +6,7 @@ do
   function _ramfs:_resolve(path, parent)
     local segments = k.common.split_path(path)
     local current = self.tree
+    
     for i=1, #segments - (parent and 1 or 0), 1 do
       if not current.children then
         return nil, k.errno.ENOTDIR
@@ -15,15 +16,18 @@ do
         return nil, k.errno.ENOENT
       end
     end
+    
     return current, parent and segments[#segments]
   end
 
   function _ramfs:stat(path)
     checkArg(1, path, "string")
+    
     local fblk, err = self:_resolve(path)
     if not fblk then
       return nil, err
     end
+    
     return {
       ino = -1,
       mode = fblk.mode,
@@ -44,10 +48,12 @@ do
     checkArg(1, file, "string")
     checkArg(2, flags, "table")
     checkArg(3, mode, "number", "nil")
+    
     local node, err = self:_resolve(file)
     if not node then
       if flags.creat then
         checkArg(3, mode, "number")
+    
         local parent, name = self:_resolve(file, true)
         if not parent then
           return nil, name
@@ -71,6 +77,7 @@ do
         return nil, err
       end
     end
+    
     local n = #fds + 1
     fds[n] = {
       ptr = 0,
@@ -79,44 +86,53 @@ do
       read = (flags.rdwr or flags.rdonly) and node.reader and node.reader(n),
       write = (flags.rdwr or flags.wronly) and node.writer and node.writer(n),
     }
+    
     return n
   end
 
   function _ramfs:read(fd, count)
     checkArg(1, fd, "number")
     checkArg(2, count, "number")
+    
     local _fd = fds[fd]
     if not (_fd and (_fd.flags.rdwr or fd.flags.rdonly)) then
       return nil, k.errno.EBADF
     end
+    
     if _fd.read then
       return _fd.read(fd, _fd.ptr, count)
     end
+    
     if fd.ptr < #fd.data then
       local n = math.min(#fd.data, fd.ptr + count)
       local ret = fd.data:sub(fd.ptr, n)
       fd.ptr = n + 1
       return ret
     end
+
     return nil
   end
 
   function _ramfs:write(fd, data)
     checkArg(1, fd, "number")
     checkArg(2, data, "string")
+    
     local _fd = fds[fd]
     if not (_fd and (_fd.flags.rdwr or fd.flags.wronly)) then
       return nil, k.errno.EBADF
     end
+    
     if _fd.write then
       return _fd.write(fd, _fd.ptr, data)
     end
+    
     if fd.ptr == #fd.data then
       fd.data = fd.data .. data
       fd.ptr = #fd.data
     else
       fd.data = fd.data:sub(0, fd.ptr) .. data .. fd.data:sub(fd.ptr+1)
     end
+    
     return true
   end
 
@@ -124,31 +140,39 @@ do
     checkArg(1, fd, "number")
     checkArg(2, whence, "string")
     checkArg(3, offset, "number")
+    
     local _fd = fds[fd]
     if not _fd then
       return nil, k.errno.EBADF
     end
+    
     if _fd.seek then
       return _fd.seek(fd, _fd.ptr, whence, offset)
     end
+    
     whence = (whence == "set" and 0)
           or (whence == "cur" and _fd.ptr)
           or (whence == "end" and #_fd.data)
+    
     if whence + offset > #_fd.data then
       return nil, k.errno.EOVERFLOW
     end
+    
     _fd.ptr = whence + offset
     return _fd.ptr
   end
 
   function _ramfs:close(fd)
     checkArg(1, fd, "number")
+    
     if not fds[fd] then
       return nil, k.errno.EBADF
     end
+    
     if fds[fd].close then
       fds[fd].close(fd, fds[fd].ptr)
     end
+    
     fds[fd] = nil
     return true
   end
@@ -156,9 +180,12 @@ do
   function _ramfs:mkdir(path, mode)
     checkArg(1, path, "string")
     checkArg(2, mode, "number")
+
     local parent, name = self:_resolve(path, true)
     if parent.children[name] then
+      return nil, k.errno.EEXIST
     end
+
     parent.children[name] = {
       mode = k.common.fsmodes.f_directory |
               k.common.fsmodes.owner_r |
@@ -179,20 +206,25 @@ do
   function ramfs:link(old, new)
     checkArg(1, old, "string")
     checkArg(2, new, "string")
+    
     local node, err = self:_resolve(old)
     if not node then
       return nil, err
     end
+    
     local newnode, name = self:_resolve(new, true)
     if not newnode then
       return nil, err
     end
+    
     if not newnode.children then
       return nil, k.errno.ENOTDIR
     end
+    
     if newnode.children[name] then
       return nil, k.errno.EEXIST
     end
+    
     newnode.children[name] = node
     node.nlink = node.nlink + 1
     return true
@@ -200,21 +232,41 @@ do
 
   function ramfs:unlink(path)
     checkArg(1, path, "string")
+    
     local parent, name = self:_resolve(path, true)
     if not parent then
       return nil, name
     end
+    
     if not parent.children then
       return nil, k.errno.ENOTDIR
     end
+    
     if not parent.children[name] then
       return nil, k.errno.ENOENT
     end
+    
     parent.children[name].nlink = parent.children[name].nlink - 1
     if parent.children[name].nlink == 0 then
       parent.children[name] = nil
     end
+    
     return true
+  end
+
+  function ramfs:list(path)
+    checkArg(1, path, "string")
+    local node, err = self:_resolve(path)
+    if not node then
+      return nil, err
+    end
+
+    local flist = {}
+    for k, v in pairs(node.children) do
+      flist[#flist+1] = k
+    end
+
+    return flist
   end
 
   function _ramfs.new()
