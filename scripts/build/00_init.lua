deb2f669095ac1081705c45277488afa4eac3f96
Ocawesome101 <ocawesome101@gmail.com>
2022-01-23 15:56:50 -0500

    add help message when .config isn't found

--- scripts/build/00_init.lua
+++ scripts/build/00_init.lua
@@ -3,5 +3,14 @@
 local conflib = require("scripts/requirable/config")
 local log = require("scripts/requirable/logger")
 
+local handle = io.open(".config", "r")
+if handle then handle:close() else
+  print(log.red .. "==> " .. log.white .. "Build configuration not found.")
+  print(log.red .. "==> " .. log.white .. "Copy " .. log.yellow ..
+    ".defconfig" .. log.white .. " to " .. log.yellow .. ".config" ..
+    log.white .. " and edit it.")
+  os.exit(1)
+end
+
 print(log.indent .. "Loading " .. log.yellow .. ".config" .. log.white)
 _G.bconf = conflib.load(".config")
