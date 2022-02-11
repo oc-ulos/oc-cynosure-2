324700eb005c167633c81fc8e9a5703d71bac101
Ocawesome101 <ocawesome101@gmail.com>
2022-02-11 00:24:31 -0500

    stubs

--- src/fs/managed.lua
+++ src/fs/managed.lua
@@ -20,4 +20,74 @@ printk(k.L_INFO, "fs/managed")
 
 do
   local _node = {}
+
+  -- file attributes are stored as 'key:value' pairs
+  -- these are:
+  --  uid:number
+  --  gid:number
+  --  mode:number
+  --  devmaj:number present if file is block/chardev
+  --  devmin:number present if file is block/chardev
+  --  created:number
+
+  -- take the attribute file data and return a table
+  local function load_attributes()
+  end
+
+  -- take a table of attributes and return file data
+  local function dump_attributes()
+  end
+
+  -- get the attributes of a specific file
+  function _node:get_attributes(file)
+  end
+
+  -- set the attributes of a specific file
+  function _node:set_attributes(file)
+  end
+
+  function _node:exists()
+  end
+
+  function _node:stat()
+  end
+
+  function _node:chmod()
+  end
+
+  function _node:chown()
+  end
+  
+  function _node:link()
+  end
+
+  function _node:unlink()
+  end
+  
+  function _node:mkdir()
+  end
+
+  function _node:opendir()
+  end
+
+  function _node:readdir()
+  end
+
+  function _node:open()
+  end
+
+  function _node:read()
+  end
+  
+  function _node:write()
+  end
+  
+  function _node:seek()
+  end
+
+  -- this function does nothing
+  function _node:flush() end
+
+  function _node:close()
+  end
 end
