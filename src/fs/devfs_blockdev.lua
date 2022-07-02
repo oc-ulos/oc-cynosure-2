62f7d26bba60bd0b1598627497d4f652dd02844d
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-07-02 08:18:49 +0700

    Convert to 2 space tabs

--- src/fs/devfs_blockdev.lua
+++ src/fs/devfs_blockdev.lua
@@ -19,77 +19,77 @@
 printk(k.L_INFO, "fs/devfs_blockdev")
 
 do
-    local handler = {}
+  local handler = {}
 
-    function k.devfs.register_blockdev(devtype, callbacks)
-        handler[devtype] = callbacks
+  function k.devfs.register_blockdev(devtype, callbacks)
+    handler[devtype] = callbacks
+  end
+
+  local function comp_added(_, addr, t)
+    printk(k.L_DEBUG, ("component_added: %s %s"):format(addr, t))
+    if handler[t] then
+      local name, device = handler[t].init(addr)
+      if name then
+        k.devfs.register_device(name, device)
+      end
     end
+  end
 
-    local function comp_added(_, addr, t)
-        printk(k.L_DEBUG, ("component_added: %s %s"):format(addr, t))
-        if handler[t] then
-            local name, device = handler[t].init(addr)
-            if name then
-                k.devfs.register_device(name, device)
+  local function comp_removed(_, addr, t)
+    printk(k.L_DEBUG, ("component_removed: %s %s"):format(addr, t))
+    if handler[t] then
+      local name = handler[t].destroy(addr)
+      if name then
+        k.devfs.unregister_device(name)
+      end
+    end
+  end
+
+  do
+    local present = false
+
+    k.devfs.register_blockdev("eeprom", {
+      init = function(addr)
+        if not present then
+          present = true
+          local eeprom = component.proxy(addr)
+          return "eeprom", {
+            stat = function()
+              return {
+                dev = -1,
+                ino = -1,
+                mode = 0x6000 + k.perm_string_to_bitmap("rw-rw----"),
+                nlink = 1,
+                uid = 0,
+                gid = 0,
+                rdev = -1,
+                size = eeprom.getSize(),
+                blksize = eeprom.getSize(), -- Idk the difference between this and size
+                atime = 0,
+                ctime = 0,
+                mtime = 0
+              }
             end
+          }
         end
-    end
-
-    local function comp_removed(_, addr, t)
-        printk(k.L_DEBUG, ("component_removed: %s %s"):format(addr, t))
-        if handler[t] then
-            local name = handler[t].destroy(addr)
-            if name then
-                k.devfs.unregister_device(name)
-            end
+      end,
+      destroy = function(addr)
+        if present then
+          present = false
+          return "eeprom"
         end
-    end
+      end,
+    })
+  end
 
-    do
-        local present = false
+  k.blacklist_signal("component_added")
+  k.blacklist_signal("component_removed")
 
-        k.devfs.register_blockdev("eeprom", {
-            init = function(addr)
-                if not present then
-                    present = true
-                    local eeprom = component.proxy(addr)
-                    return "eeprom", {
-                        stat = function()
-                            return {
-                                dev = -1,
-                                ino = -1,
-                                mode = 0x6000 + k.perm_string_to_bitmap("rw-rw----"),
-                                nlink = 1,
-                                uid = 0,
-                                gid = 0,
-                                rdev = -1,
-                                size = eeprom.getSize(),
-                                blksize = eeprom.getSize(), -- Idk the difference between this and size
-                                atime = 0,
-                                ctime = 0,
-                                mtime = 0
-                            }
-                        end
-                    }
-                end
-            end,
-            destroy = function(addr)
-                if present then
-                    present = false
-                    return "eeprom"
-                end
-            end,
-        })
-    end
+  k.add_signal_handler("component_added", comp_added)
 
-    k.blacklist_signal("component_added")
-    k.blacklist_signal("component_removed")
+  k.add_signal_handler("component_removed", comp_removed)
 
-    k.add_signal_handler("component_added", comp_added)
-
-    k.add_signal_handler("component_removed", comp_removed)
-
-    for addr, ctype in component.list() do
-        comp_added(nil, addr, ctype)
-    end
+  for addr, ctype in component.list() do
+    comp_added(nil, addr, ctype)
+  end
 end
