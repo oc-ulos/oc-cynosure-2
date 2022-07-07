65041c61dc3286e3687fd687c3811fde68ae2fde
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-07-08 02:13:49 +0700

    Initial work on drive blockdev files

--- src/fs/devfs_blockdev.lua
+++ src/fs/devfs_blockdev.lua
@@ -107,6 +107,51 @@ do
     })
   end
 
+  do
+    local drives = {}
+    local byaddress = {}
+
+    k.devfs.register_blockdev("drive", {
+      init = function(addr)
+        local index = 0
+        while drives[index] do
+          index = index + 1
+        end
+
+        local letter = string.char(string.byte("a") + index)
+        local proxy = component.proxy(addr)
+        drives[index] = true
+        byaddress[addr] = index
+
+        return ("hd%s"):format(letter), {
+          stat = function()
+            local size = proxy.getCapacity()
+            return {
+              dev = -1,
+              ino = -1,
+              mode = 0x6000 + k.perm_string_to_bitmap("rw-rw----"),
+              nlink = 1,
+              uid = 0,
+              gid = 0,
+              rdev = -1,
+              size = size,
+              blksize = 512,
+              atime = 0,
+              ctime = 0,
+              mtime = 0
+            }
+          end,
+        }
+      end,
+      destroy = function(addr)
+        local letter = string.char(string.byte("a") + byaddress[addr])
+        drives[byaddress[addr]] = nil
+        byaddress[addr] = nil
+        return ("hd%s"):format(letter)
+      end
+    })
+  end
+
   k.blacklist_signal("component_added")
   k.blacklist_signal("component_removed")
 
